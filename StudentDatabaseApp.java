51c2189f5b9d3a9964d986e4791b55cb4b310098
pranotitonge <142015019+pranotitonge@users.noreply.github.com>
2024-08-06 14:59:24 +0530

    Add files via upload

---
+++ StudentDatabaseApp.java
@@ -0,0 +1,23 @@
+package studentdatabaseapp;
+
+import java.util.Scanner;
+
+public class StudentDatabaseApp {
+
+	public static void main(String[] args) {
+     System.out.print("Enter number of new students to enroll:");
+     Scanner in = new Scanner(System.in);
+     int numOfStudents = in.nextInt();
+     Student[] students = new Student[numOfStudents];
+     
+    for (int n =0; n < numOfStudents; n++) {
+    	 students[n] = new Student();
+    	 students[n].enroll();
+    	 students[n].payTuition();
+    }
+    for (int n =0; n < numOfStudents; n++) {
+    System.out.println(students[n].toString());
+    
+	}
+	}}
+
